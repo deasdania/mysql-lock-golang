88b58949e1c282b6d2e49db98e53a80a8517b641
dea.dania <dea.dania@brank.as>
2023-02-25 07:43:21 +0700

    prettier the codes

--- main.go
+++ main.go
@@ -1,77 +1,40 @@
 package main
 
-
-
 import (
+	"encoding/json"
 
+	"fmt"
 
-
- "encoding/json"
-
-"fmt"
-
-"net/http" 
-
-
-
-)  
-
-
+	"net/http"
+)
 
 func main() {
 
-    http.HandleFunc("/savings", httpHandler)        
+	http.HandleFunc("/savings", httpHandler)
 
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 
 }
 
+func httpHandler(w http.ResponseWriter, req *http.Request) {
 
+	params := map[string]interface{}{}
+	response := map[string]interface{}{}
 
-func httpHandler(w http.ResponseWriter, req *http.Request) {  
+	var err error
+	err = json.NewDecoder(req.Body).Decode(&params)
+	response, err = addEntry(params)
 
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "  ")
+	if err != nil {
+		response = map[string]interface{}{
+			"error": err.Error(),
+		}
+	}
 
-
-    params := map[string]interface{}{}
-
-
-
-    response := map[string]interface{}{}
-
-    var err error
-
-
-
-    err = json.NewDecoder(req.Body).Decode(&params)
-
-
-
-    response, err = addEntry(params)            
-
-
-
-    enc := json.NewEncoder(w)
-
-    enc.SetIndent("", "  ")
-
-
-
-    if err != nil {
-
-        response = map[string]interface{}{
-
-                       "error": err.Error(),
-
-                   }
-
-    } 
-
-
-
-    if encodingErr := enc.Encode(response); encodingErr != nil {
-
-        fmt.Println("{ error: " + encodingErr.Error() + "}")
-
-    }
+	if encodingErr := enc.Encode(response); encodingErr != nil {
+		fmt.Println("{ error: " + encodingErr.Error() + "}")
+	}
 
 }
